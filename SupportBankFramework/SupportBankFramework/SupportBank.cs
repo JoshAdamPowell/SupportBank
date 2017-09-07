4da4237ae06f290ccb29183d58e16d289f6444e7
Josh Powell <Josh.Powell@softwire.com>
2017-09-07 15:10:06 +0100

    Separated into functions. cleaner flow.

--- SupportBankFramework/SupportBankFramework/SupportBank.cs
+++ SupportBankFramework/SupportBankFramework/SupportBank.cs
@@ -24,10 +24,98 @@ namespace SupportBankFramework
 
     class SupportBank
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
 
             string[] rawData = System.IO.File.ReadAllLines(@"C:\Work\Training\supportbank\Transactions2014.csv");
+
+            var transactionList = CreateTransactionList(rawData);
+
+
+            var accountLog = CreateAccountLog(transactionList);
+
+            Console.WriteLine("Please enter command: (List All) or (List [Account])");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "List All")
+            {
+                ListAll(accountLog);
+            }
+            if (userInput != "List All" && userInput.Contains("List"))
+            {
+                ListSpecific(userInput, transactionList);
+            }
+            Console.ReadKey();
+
+        }
+
+
+        public static void ListAll(Dictionary<string, float> dictionary)
+        {
+            foreach (var kvp in dictionary)
+            {
+                Console.WriteLine(" Account name: " + kvp.Key + " -- Account Value: " + kvp.Value);
+            }
+        }
+
+
+        public static void ListSpecific(string userInput, List<Transaction> listOfTransactions)
+        {
+            int spaceLocation = userInput.IndexOf(" ");
+            string accountName = userInput.Substring(spaceLocation + 1);
+
+
+            foreach (var currentTransaction in listOfTransactions)
+            {
+                if (currentTransaction.accountFrom == accountName)
+                {
+                    string transactionString = string.Format("{0} Paid {1} to {2} for {3}.", currentTransaction.dates, currentTransaction.amount,
+                        currentTransaction.accountTo, currentTransaction.description);
+                    Console.WriteLine(transactionString);
+                }
+
+                if (currentTransaction.accountTo == accountName)
+                {
+                    string transactionString = string.Format("{0} Received {1} from {2} for {3}.", currentTransaction.dates, currentTransaction.amount,
+                        currentTransaction.accountFrom, currentTransaction.description);
+                    Console.WriteLine(transactionString);
+                }
+
+            }
+        }
+
+        public static Dictionary<string, float> CreateAccountLog(List<Transaction> transactionList)
+        {
+            var accountLog = new Dictionary<string, float>();
+
+            foreach (var currentTransaction in transactionList)
+            {
+                if (accountLog.ContainsKey(currentTransaction.accountFrom))
+                {
+                    accountLog[currentTransaction.accountFrom] = accountLog[currentTransaction.accountFrom] - currentTransaction.amount;
+                }
+                else
+                {
+                    accountLog.Add(currentTransaction.accountFrom, -currentTransaction.amount);
+                }
+            }
+
+            foreach (var currentTransaction in transactionList)
+            {
+                if (accountLog.ContainsKey(currentTransaction.accountTo))
+                {
+                    accountLog[currentTransaction.accountTo] = accountLog[currentTransaction.accountTo] + currentTransaction.amount;
+                }
+                else
+                {
+                    accountLog.Add(currentTransaction.accountTo, currentTransaction.amount);
+                }
+            }
+            return accountLog;
+        }
+
+        public static List<Transaction> CreateTransactionList(string[] rawData)
+        {
             int dataLength = rawData.Length;
 
             List<Transaction> transactionList = new List<Transaction>();
@@ -55,78 +143,9 @@ namespace SupportBankFramework
                 transactionList.Add(currentTransaction);
             }
 
-
-
-            var accountLog = new Dictionary<string, float>();
-
-            foreach (var currentTransaction in transactionList)
-            {
-                if (accountLog.ContainsKey(currentTransaction.accountFrom))
-                {
-                    accountLog[currentTransaction.accountFrom] = accountLog[currentTransaction.accountFrom] - currentTransaction.amount;
-
-                }
-                else
-                {
-                    accountLog.Add(currentTransaction.accountFrom, -currentTransaction.amount);
-                }
-            }
-
-            foreach (var currentTransaction in transactionList)
-            {
-                if (accountLog.ContainsKey(currentTransaction.accountTo))
-                {
-                    accountLog[currentTransaction.accountTo] = accountLog[currentTransaction.accountTo] + currentTransaction.amount;
-
-                }
-                else
-                {
-                    accountLog.Add(currentTransaction.accountTo, currentTransaction.amount);
-                }
-            }
-
-
-            Console.WriteLine("Please enter command: (List All) or (List [Account])");
-            string userInput = Console.ReadLine();
-
-            if (userInput == "List All")
-            {
-                foreach (var kvp in accountLog)
-                {
-                    Console.WriteLine(" Account name: " + kvp.Key + " -- Account Value: " + kvp.Value);
-                }
-                Console.ReadKey();
-            }
-            if (userInput != "List All" && userInput.Contains("List"))
-            {
-                int spaceLocation = userInput.IndexOf(" ");
-                string accountName = userInput.Substring(spaceLocation + 1);
-
-
-                foreach (var currentTransaction in transactionList)
-                {
-                    if (currentTransaction.accountFrom == accountName)
-                    {
-                        string transactionString = string.Format("{0} Paid {1} to {2} for {3}.", currentTransaction.dates, currentTransaction.amount,
-                            currentTransaction.accountTo, currentTransaction.description);
-                        Console.WriteLine(transactionString);
-                    }
-
-                    if (currentTransaction.accountTo == accountName)
-                    {
-                        string transactionString = string.Format("{0} Received {1} from {2} for {3}.", currentTransaction.dates, currentTransaction.amount,
-                            currentTransaction.accountFrom, currentTransaction.description);
-                        Console.WriteLine(transactionString);
-                    }
-
-                }
-
-                Console.ReadKey();
-                
-
-            }
-
+            return transactionList;
         }
+
 
     }
 }
